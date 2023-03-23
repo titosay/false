3c5fadd5073cc413ed0a40523091cc89dcba6c91
Студент ПТК <student@ptk.ru>
2023-03-23 09:36:10 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// let Hastya2 = {
+//     name: "Hastya",
+//     sayHello: function() {
+//         console.log("Привет, " + this.name + "!");
+//     } 
+// };
+
+// Hastya2.sayHello();
+
+
+// function isEven(number){
+//     var x = new Boolean(false);
+//     if( number % 2 == 0){
+//     return true;
+//     }else{
+//     return false;
+//     }
+    
+//     };
+    
+// var x = isEven(10);
+// console.log(x);
+
+
+// let person = {
+//     name: "Пётр",
+//     age: 25,
+//     city: "Новгород",
+//     sayHello: function () {
+//         console.log("Меня зовут " + this.name + ". Мне " + this.age + " лет. Я живу в городе " + this.city + ".");
+//     }
+// };
+
+// person.sayHello();
+
+
+// var arr = [4, 8, 6, 13, 12]
+// var sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+// };
+ 
+// console.log(sum);
